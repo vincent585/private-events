013cd37df72d5a417ea46e51f112393ea1af152b
Vincent Klimjack <vklimjack@gmail.com>
2023-05-05 22:14:47 +0100

    add index method to EventsController and add index view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# controller responsible for the handling of events
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def create; end
 end
