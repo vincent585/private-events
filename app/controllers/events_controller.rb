76185f129e3b5312fad2f49553dbbf01b4f28732
Vincent Klimjack <vklimjack@gmail.com>
2023-07-30 10:41:30 +0100

    implement create event
    add view for new with form
    add route for new
    add time formatting to views
    implement create method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,9 +2,26 @@
 
 # controller responsible for the handling of events
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
   def index
     @events = Event.all
   end
 
-  def create; end
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :date, :time, :creator_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[show]
-  resources :events, only: %i[index create show]
+  resources :events, only: %i[index create show new]
 
   root to: 'events#index'
 end
