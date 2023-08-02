6ddcd3cd3d4a37f6d477706d9a0b9ee84c5bc80e
Vincent Klimjack <vklimjack@gmail.com>
2023-08-02 15:56:22 +0100

    readability

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -5,19 +5,20 @@ class EventAttendancesController < ApplicationController
 
   def create
     @event = Event.find(params[:attended_event_id])
-    if EventAttendance.find_by(attendee_id: current_user.id, attended_event_id: @event.id).nil?
-      @event_attendance = EventAttendance.new(attended_event_id: @event.id, attendee_id: current_user.id)
-      if @event_attendance.save
-        flash[:notice] = "You're signed up for the event!"
 
-        redirect_to event_path(@event.id)
-      else
-        flash[:notice] = "Something went wrong"
-
-        redirect_to event_path(@event.id)
-      end
-    else
+    unless EventAttendance.find_by(attendee_id: current_user.id, attended_event_id: @event.id).nil?
       flash[:notice] = "You're already attending this event!"
+      redirect_to event_path(@event.id)
+    end
+
+    @event_attendance = EventAttendance.new(attended_event_id: @event.id, attendee_id: current_user.id)
+    if @event_attendance.save
+      flash[:notice] = "You're signed up for the event!"
+
+      redirect_to event_path(@event.id)
+    else
+      flash[:notice] = "Something went wrong"
+
       redirect_to event_path(@event.id)
     end
   end
