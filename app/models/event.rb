98ec3a81979dbd4e200af1fe99d120c9fc6f8fc7
Vincent Klimjack <vklimjack@gmail.com>
2023-08-05 13:26:49 +0100

    fix rubocop suggestions, add past and upcoming class methods for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances, source: :attendee
+
+  def self.past
+    where('date < ?', Date.today)
+  end
+
+  def self.upcoming
+    where('date >= ?', Date.today)
+  end
 end
