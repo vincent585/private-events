14fc71d60593d249f7bcd4b35dc9e26a193833e4
Vincent Klimjack <vklimjack@gmail.com>
2023-08-10 08:35:47 +0100

    add edit functionality and protect against non-creator editing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@
 
 # controller responsible for the handling of events
 class EventsController < ApplicationController
+  before_action :require_permission, only: %i[edit destroy]
   def index
     @events = Event.all
   end
@@ -32,7 +33,25 @@ class EventsController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def event_params
     params.require(:event).permit(:name, :description, :location, :date, :time, :creator_id)
+  end
+
+  def require_permission
+    redirect_to root_path if current_user != Event.find(params[:id]).creator
   end
 end
